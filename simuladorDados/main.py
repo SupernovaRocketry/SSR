21c24e149386cba370567ddfec2df2cba1008a58
ThiiD <64203938+ThiiD@users.noreply.github.com>
2021-05-13 02:25:44 -0300

    Update main.py
    Inicio do código de comunicação via socket, em que simuladorDados funciona como client.

--- simuladorDados/main.py
+++ simuladorDados/main.py
@@ -1,1 +1,18 @@
-print("Hello, World!")
+# simuladorDados funcionará como um client socket.
+
+# Incluindo bibliotecas
+import socket
+
+HOST = "127.0.0.1"
+PORT = 500
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+dest = (HOST, PORT)
+tcp.connect(dest)
+print("Para sair use CTRL+X\n")
+msg = input("Digite uma mensagem")
+
+while(msg != '\x18'):
+    tcp.send(msg)
+    msg = input("Digite uma nova mensagem")
+
+tcp.close()
