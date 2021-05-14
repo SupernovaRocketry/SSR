70fa0e6512e267475c8c479dd3a111d81c133dcc
ThiiD <64203938+ThiiD@users.noreply.github.com>
2021-05-14 14:10:02 -0300

    Ambiente Virtual
    Inclusão de um ambiente virtual e adição da biblioteca Kivy.

--- supervisorio/main.py
+++ supervisorio/main.py
@@ -2,13 +2,14 @@
 
 # Importando bibliotecas
 import socket
+import kivy
+from kivy.app import App
 
-HOST = ''
-PORT = 5000
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-orig = (HOST, PORT)
-
-tcp.bind(orig)
+HOST = ''                                                   # Define o ip com para esse programa. No caso, será utilizado o ip de feedback "127.0.0.1"
+PORT = 5000                                                 # Define a porta de comunicação
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     # Cria o socket com o protocolo com o qual se deseja comunicar. No caso, TCP/IP
+orig = (HOST, PORT)                                         # Lista contendo o ip e porta counicação
+tcp.bind(orig)                                              # 
 tcp.listen(1)
 
 while True:
