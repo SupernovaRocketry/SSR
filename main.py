a7de6408ddc6fe77d3c6e77d327d1321cefbc52c
ThiiD <thiago.saber@engenharia.ufjf.br>
2021-05-27 01:45:06 -0300

    Inclusão Kivy
    Inclusão do framework kivy com alguns testes e adição do requirements.txt.

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
+from typing import Text
+from kivy.uix.behaviors import button
 from cliente import Cliente
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+class BasicApp(App):
+    """
+    Aplicativo Basico Kivy
+    """
+    def build(self):
+        """
+        Construir o aplicativo 
+        """
+        layout = BoxLayout(orientation = 'vertical')
+        lb = Label(text = 'label 1')
+        bt = Button(text = "Botão 1")
+        layout.add_widget(bt)
+        layout.add_widget(lb)
+        layout2 = BoxLayout(orientation = 'horizontal')
+        lb2 = Label(text= 'label 2')
+        lb3 = Label(text = 'label 3')
+        layout2.add_widget(lb2)
+        layout2.add_widget(lb3)
+        layout.add_widget(layout2)
+        return layout
+
+if __name__ == '__main__':
+    BasicApp().run()
+
+
 
 c = Cliente('localhost', 9000)
 c.start()
