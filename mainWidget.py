5b16ed929d95926c4a6f4232a32a38b8ee59fdc2
ThiiD <thiago.saber@engenharia.ufjf.br>
2021-05-29 16:35:49 -0300

    Estruturação inicial Kivy

--- mainWidget.py
+++ mainWidget.py
@@ -3,6 +3,7 @@ import kivy
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 
 
